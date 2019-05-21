7585f1b84ebe4b9388d878ed9aabd8362f47bbc7
Sam <samuelzheng11@gmail.com>
2019-05-21 12:59:22 +1200

    get print vert houses method

--- src/kalah/IO/BoardLayout.java
+++ src/kalah/IO/BoardLayout.java
@@ -1,7 +1,9 @@
 package kalah.IO;
 
+import kalah.game_objects.GameState;
+
 import java.util.List;
 
 public interface BoardLayout {
-    List<String> getPrintableBoard();
+    List<String> getPrintableBoardFor(GameState gameState);
 }
--- src/kalah/IO/VerticalBoardLayout.java
+++ src/kalah/IO/VerticalBoardLayout.java
@@ -1,14 +1,29 @@
 package kalah.IO;
 
+import kalah.game_objects.GameState;
+import kalah.misc.Constants;
+import kalah.misc.PlayerId;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class VerticalBoardLayout implements BoardLayout {
 
     @Override
-    public List<String> getPrintableBoard() {
+    public List<String> getPrintableBoardFor(GameState gameState) {
         List<String> boardRepresentation = new ArrayList<>();
 
+        boardRepresentation.add("+-------+-------+");
+        boardRepresentation.add("|       | P2 " + formatWhiteSpaceForNumber(gameState.getPlayerStoreScore(PlayerId.PLAYER_2.getPlayerValue())) +" |");
 
+        for (int i = 0; i < Constants.DEFAULT_NUMBER_OF_HOUSES; i++) {
+            boardRepresentation.add(printVerticalHouses(gameState, i));
+        }
+
+        boardRepresentation.add("+-------|-------+");
+        boardRepresentation.add("| P1 " + formatWhiteSpaceForNumber(gameState.getPlayerStoreScore(PlayerId.PLAYER_1.getPlayerValue())) + " |       |");
+        boardRepresentation.add("+-------+-------+");
+
+        return boardRepresentation;
     }
 }
